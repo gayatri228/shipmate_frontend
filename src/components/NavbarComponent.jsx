87fce7c1175818b1350e19b61f4751d831c48a13
vyankatesh <vrnyati@gmail.com>
2023-04-10 23:59:32 +0530

    navbar

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+
+const NavbarComponent = () => {
+  return <div className={classes.container}>ShipMate</div>;
+};
+
+export default NavbarComponent;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./HomePgae.module.css";
+
+const HomePage = () => {
+  return (
+    <div className={classes.container}>
+      <form>
+        <label>Name: </label>
+        <input />
+        <label>Name: </label>
+        <input />
+        <label>Name: </label>
+        <input />
+        <label>Name: </label>
+        <input />
+        <label>Name: </label>
+        <input />
+      </form>
+    </div>
+  );
+};
+
+export default HomePage;
